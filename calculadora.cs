ae7c8753555c89471976dd89abe712718312add9
unknown <maguom87@gmail.com>
2025-07-28 20:34:15 -0400

    Funcionalidad suma implementada

--- calculadora.cs
+++ calculadora.cs
@@ -4,5 +4,14 @@ using System;
 class Program {
     static void Main() {
         Console.WriteLine("Bienvenido a la Calculadora Básica");
+        Console.Write("Ingrese primer número: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese segundo número: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Resultado: {a + b}")
     }
+
 }
+
